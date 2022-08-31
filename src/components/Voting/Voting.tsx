db03e77fe8b9c308a50c517e0f34cee9b3433f1a
shurik <86realmadrid86@gmail.com>
2022-08-31 23:58:27 +0300

    added a new common back button component

--- src/components/Voting/Voting.tsx
+++ src/components/Voting/Voting.tsx
@@ -1,13 +1,14 @@
-import React      from 'react'
-import s          from './Voting.module.scss'
-import { Search } from '../Search'
-import { Button } from '../common/Button'
+import React                  from 'react'
+import s                      from './Voting.module.scss'
+import { Search }             from '../Search'
+import { BackButton, Button } from '../common/Buttons'
 
 
 const Voting: React.FC = () => {
 	return (
 		<div className={ s.voting }>
 			<Search/>
+			<BackButton/>
 			<Button name='Voting'/>
 		</div>
 	)
