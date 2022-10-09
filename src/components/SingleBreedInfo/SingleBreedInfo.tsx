ceeac0e7ae27668f3bdb15896c21d1176390ed97
AlexP <86realmadrid86@gmail.com>
2022-10-09 20:45:37 +0300

    added tow new components: refactor

--- src/components/SingleBreedInfo/SingleBreedInfo.tsx
+++ src/components/SingleBreedInfo/SingleBreedInfo.tsx
@@ -2,29 +2,22 @@ import React, { useEffect } from 'react'
 import s                    from './SingleBreedInfo.module.scss'
 import qs                   from 'qs'
 
-import { Swiper, SwiperSlide }                               from 'swiper/react'
-import SwiperCore, { Pagination, Autoplay, EffectCoverflow } from 'swiper'
-import 'swiper/scss'
-import 'swiper/scss/navigation'
-import 'swiper/scss/pagination'
-
 import { useSelector }        from 'react-redux'
 import { useAppDispatch }     from '../../redux/store'
 import { fetchSingleBreed }   from '../../redux/Breeds/asyncActions'
 import { selectBreeds }       from '../../redux/Breeds/selectors'
 import { setActiveBtn }       from '../../redux/voting/slice'
 import { setActiveBreedName } from '../../redux/Breeds/slice'
-import { TSingleBreed }       from '../../redux/Breeds/types'
 
 import { BreadCrumbs, Spinner } from '../common'
+import Slider                   from './Slider'
+import SliderDesc               from './SliderDesc'
 
 
 const SingleBreedInfo: React.FC = () => {
 	const dispatch = useAppDispatch()
 	const { singleBreed, status, activeBreedName } = useSelector(selectBreeds)
 	const emptyData = singleBreed.length === 0
-
-	SwiperCore.use([ Pagination, EffectCoverflow, Autoplay ])
 
 	useEffect(() => {
 		dispatch(setActiveBreedName(singleBreed[0]?.name))
@@ -48,50 +41,8 @@ const SingleBreedInfo: React.FC = () => {
 				{
 					singleBreed.length > 0 && status === 'success'
 						? <>
-							<Swiper
-								className={ s.breed__main_slider }
-								pagination={ { clickable: true } }
-								effect='coverflow'
-								coverflowEffect={ { rotate: 100, slideShadows: false } }
-								slidesPerView={ 1 }
-								wrapperTag='ul'
-								autoplay={ { delay: 5000, disableOnInteraction: false } }>
-								{
-									singleBreed.map((sliderEl: TSingleBreed) => (
-										<SwiperSlide tag='li' className={ s.breed__img_wr } key={ sliderEl.image }>
-											<img src={ sliderEl.image } alt=''/>
-										</SwiperSlide>
-									))
-								}
-							</Swiper>
-							<div className={ s.breed__feature_wr }>
-								<h1 className={ s.breed__title }>{ singleBreed[0].name }</h1>
-								<h1 className={ s.breed__subTitle }>{ singleBreed[0].desc }</h1>
-								<div>
-									<div className={ s.breed__desc_wr }>
-
-										<ul className={ s.breed__left }>
-											<li className={ s.breed__desc }>
-												<span className={ s.breed__desc__span }>Temperament: </span><br/>
-												{ singleBreed[0].temperament }
-											</li>
-										</ul>
-
-										<ul className={ s.breed__right }>
-											<li className={ s.breed__desc }>
-												<span className={ s.breed__desc__span }>Origin: </span>{ singleBreed[0].origin }
-											</li>
-											<li className={ s.breed__desc }>
-												<span className={ s.breed__desc__span }>Weight: </span>{ singleBreed[0].weight } kg
-											</li>
-											<li className={ s.breed__desc }>
-												<span className={ s.breed__desc__span }>Life span: </span>{ singleBreed[0].life_span } years
-											</li>
-										</ul>
-
-									</div>
-								</div>
-							</div>
+							<Slider singleBreed={ singleBreed }/>
+							<SliderDesc singleBreed={ singleBreed }/>
 						</>
 						: <Spinner/>
 				}
