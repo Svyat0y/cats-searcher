886892896f6e3195e3b77b0d70663e03e4a88b14
AlexP <86realmadrid86@gmail.com>
2022-10-15 20:53:44 +0300

    refactoring

--- src/components/common/BreedSelect/BreedSelect.tsx
+++ src/components/common/BreedSelect/BreedSelect.tsx
@@ -1,19 +1,16 @@
 import React from 'react'
 
-import Select       from 'react-select'
-import makeAnimated from 'react-select/animated'
+import Select from 'react-select'
 
 import { TBreedSelect } from '../../Breeds/types'
 
 
-const animatedComponents = makeAnimated()
 const BreedSelect: React.FC<TBreedSelect> = ({ options, status }) => {
 
 
 	return (
 		<div className='selectBreedContainer'>
 			<Select
-				components={ animatedComponents }
 				classNamePrefix='breed breedSelect'
 				placeholder='All breeds'
 				options={ options }
