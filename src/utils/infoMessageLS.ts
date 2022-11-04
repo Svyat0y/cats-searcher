5dd1e36117278a3505adba4f02b18cf9954e4ab9
AlexP <86realmadrid86@gmail.com>
2022-11-04 16:17:32 +0200

    refactoring

--- src/utils/infoMessageLS.ts
+++ src/utils/infoMessageLS.ts
@@ -3,19 +3,21 @@ import { AppDispatch }      from '../redux/store'
 import { setInfoMessage }   from '../redux/voting/slice'
 
 
-export const setLsMessages = (message: TInfoInfoMessage) => {
+const getData = () => {
 	const data = localStorage.getItem('voteMessages')
-	const messages = data ? JSON.parse(data) : []
+	return data ? JSON.parse(data) : []
+}
 
+export const setLsMessages = (message: TInfoInfoMessage) => {
+	const messages = getData()
 	messages.unshift(message)
+
 	if (messages.length > 4) messages.pop()
 	const json = JSON.stringify(messages)
 	localStorage.setItem('voteMessages', json)
 }
 
 export const getLsMessages = (dispatch: AppDispatch) => {
-	const data = localStorage.getItem('voteMessages')
-	const messages = data ? JSON.parse(data) : []
-
+	const messages = getData()
 	dispatch(setInfoMessage(messages))
 }
