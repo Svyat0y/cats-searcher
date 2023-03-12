910ba1a083535b1de9cdc2c081c8cc029a5d6523
Svyat0y <86realmadrid86@gmail.com>
2023-03-12 14:47:22 +0200

    upd

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,20 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
+	"stories": [
+		"../src/**/*.stories.mdx",
+		"../src/**/*.stories.@(js|jsx|ts|tsx)"
+	],
+	"addons": [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-interactions",
+		"@storybook/preset-create-react-app"
+	],
+	"framework": "@storybook/react",
+	"core": {
+		"builder": "@storybook/builder-webpack5"
+	},
+	"webpackFinal": async (config, {configType}) => {
+		config.outputDir = 'storybook-static';
+		return config;
+	},
 }
