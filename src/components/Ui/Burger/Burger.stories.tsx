83d8611f1e592248b52c51279cc371e418fc5427
Svyat0y <86realmadrid86@gmail.com>
2023-03-13 20:30:50 +0200

    added burger story

---
+++ src/components/Ui/Burger/Burger.stories.tsx
@@ -0,0 +1,35 @@
+import Burger                            from './Burger'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { Provider }                      from 'react-redux'
+import { store }                         from '../../../redux/store'
+import { CSSProperties }                 from 'react'
+import '../../../scss/decoratorsStorybook.scss'
+
+
+export default {
+	title: 'components/Ui/buttons',
+	component: Burger,
+	argTypes: {
+		theme: {
+			control: { type: 'radio', options: [ 'light', 'dark' ] }
+		}
+	},
+	decorators: [
+		(Story, { args }) => {
+			const backgroundColor = args.theme === 'light' ? '#FFFFFF' : '#292929'
+			const style: CSSProperties & Record<string, string> = { '--bg_white-grey': backgroundColor }
+
+			return (
+				<Provider store={ store }>
+					<div style={ style } className='burger-decorator'><Story/></div>
+				</Provider>
+			)
+		}
+	],
+} as ComponentMeta<typeof Burger>
+
+const Template: ComponentStory<typeof Burger> = (args) => <Burger { ...args } />
+export const DefaultBurger = Template.bind({})
+
+
+
